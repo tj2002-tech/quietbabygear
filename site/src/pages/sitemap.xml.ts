5fc82118770c7cc971b437d997c512c4f7f8fe0b
maixx <603528999@qq.com>
2026-06-19 12:21:11 +0800

    Publish Grownsy warning card notes

--- site/src/pages/sitemap.xml.ts
+++ site/src/pages/sitemap.xml.ts
@@ -1,6 +1,7 @@
 const pages = [
   ["/", "2026-06-19", "weekly", "1.0"],
   ["/tests/", "2026-06-19", "weekly", "0.9"],
+  ["/tests/grownsy-warning-card-backflow-chamber-drying/", "2026-06-19", "monthly", "0.8"],
   ["/tests/nosefrida-vs-grownsy-at-3am/", "2026-06-19", "monthly", "0.9"],
   ["/tests/easiest-baby-nasal-aspirator-to-clean-at-3am/", "2026-06-04", "monthly", "0.6"],
   ["/methodology/", "2026-06-19", "monthly", "0.8"],
@@ -32,4 +33,3 @@ ${pages
     }
   });
 }
-
